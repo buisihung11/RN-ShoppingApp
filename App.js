be3bff99e38b96446976deb2611bef553e098661
buisihung11 <38785848+buisihung11@users.noreply.github.com>
2020-04-10 20:45:52 +0700

    Add error boundary

--- App.js
+++ App.js
@@ -1,16 +1,17 @@
-import React, { useState } from "react";
-import { StyleSheet, View } from "react-native";
-import { AppLoading } from "expo";
-import * as Font from "expo-font";
-import ShopNavigator from "./navigation/ShopNavigator";
-import CartProvider from "./store/reducers/cart";
-import OrderProvider from "./store/reducers/orders";
-import ProductProvider from "./store/reducers/products";
+import React, { useState } from 'react';
+import { StyleSheet, View } from 'react-native';
+import { AppLoading } from 'expo';
+import ErrorBoundary from 'react-native-error-boundary';
+import * as Font from 'expo-font';
+import ShopNavigator from './navigation/ShopNavigator';
+import CartProvider from './store/reducers/cart';
+import OrderProvider from './store/reducers/orders';
+import ProductProvider from './store/reducers/products';
 
 const fetchFonts = () => {
   return Font.loadAsync({
-    "open-sans": require("./assets/fonts/OpenSans-Regular.ttf"),
-    "open-sans-bold": require("./assets/fonts/OpenSans-Bold.ttf"),
+    'open-sans': require('./assets/fonts/OpenSans-Regular.ttf'),
+    'open-sans-bold': require('./assets/fonts/OpenSans-Bold.ttf'),
   });
 };
 
@@ -31,7 +32,9 @@ export default function App() {
     <CartProvider>
       <OrderProvider>
         <ProductProvider>
-          <ShopNavigator />
+          <ErrorBoundary>
+            <ShopNavigator />
+          </ErrorBoundary>
         </ProductProvider>
       </OrderProvider>
     </CartProvider>
@@ -40,8 +43,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
 });
