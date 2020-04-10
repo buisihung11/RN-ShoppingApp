50756d6cf02887810e93bce616a1a176514bbad8
buisihung11 <38785848+buisihung11@users.noreply.github.com>
2020-04-10 10:23:43 +0700

    Revert "Import context provider"
    This reverts commit cc2026b392b14173976ab5f49ca915cdcdd4296b.

---
+++ App.js
@@ -0,0 +1,36 @@
+import React, { useState } from 'react';
+import { StyleSheet } from "react-native";
+import { AppLoading } from 'expo';
+import * as Font from 'expo-font';
+import ShopNavigator from "./navigation/ShopNavigator";
+
+const fetchFonts = () => {
+  return Font.loadAsync({
+    "open-sans": require("./assets/fonts/OpenSans-Regular.ttf"),
+    "open-sans-bold": require("./assets/fonts/OpenSans-Bold.ttf"),
+  });
+};
+
+export default function App() {
+  const [fontLoaded, setFontLoaded] = useState(false);
+
+  if (!fontLoaded) {
+    return (
+      <AppLoading
+        startAsync={fetchFonts}
+        onFinish={() => {
+          setFontLoaded(true);
+        }}
+      />
+    );
+  }
+  return <ShopNavigator />;
+}
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
