c839861a46ecab94a739ade221fdae6d9eed5169
buisihung11 <38785848+buisihung11@users.noreply.github.com>
2020-04-10 20:44:31 +0700

    Remove async code in actions

--- store/actions/products.js
+++ store/actions/products.js
@@ -2,38 +2,14 @@ export const DELETE_PRODUCT = 'DELETE_PRODUCT';
 export const CREATE_PRODUCT = 'CREATE_PRODUCT';
 export const UPDATE_PRODUCT = 'UPDATE_PRODUCT';
 
-export const deleteProduct = productId => {
+export const deleteProduct = (productId) => {
   return { type: DELETE_PRODUCT, pid: productId };
 };
 
-export const createProduct = (title, description, imageUrl, price) => {
-  return async dispatch => {
-    // any async code you want!
-    const response = await fetch('https://rn-complete-guide.firebaseio.com/products.json', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({
-        title,
-        description,
-        imageUrl,
-        price
-      })
-    });
-
-    const resData = await response.json();
-
-    dispatch({
-      type: CREATE_PRODUCT,
-      productData: {
-        id: resData.name,
-        title,
-        description,
-        imageUrl,
-        price
-      }
-    });
+export const createProduct = (productData) => {
+  return {
+    type: CREATE_PRODUCT,
+    productData,
   };
 };
 
@@ -44,7 +20,7 @@ export const updateProduct = (id, title, description, imageUrl) => {
     productData: {
       title,
       description,
-      imageUrl
-    }
+      imageUrl,
+    },
   };
 };
