6be0bb9315bb721104a3290a79c6e0cc9ad33496
buisihung11 <38785848+buisihung11@users.noreply.github.com>
2020-04-12 22:14:14 +0700

    Add firebase package

---
+++ store/firebase.js
@@ -0,0 +1,15 @@
+import firebase from 'firebase';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyB9wYuHvHxRZ2qCL1VgJtdt4wWoA_UpvNs',
+  authDomain: 'shopping-app-native.firebaseapp.com',
+  databaseURL: 'https://shopping-app-native.firebaseio.com',
+  projectId: 'shopping-app-native',
+  storageBucket: 'shopping-app-native.appspot.com',
+  messagingSenderId: '1087717829313',
+  appId: '1:1087717829313:web:d3db753ee183c3ca140240',
+  measurementId: 'G-DNZG6S4VFM',
+};
+
+firebase.initializeApp(firebaseConfig);
+export const db = firebase.database();
