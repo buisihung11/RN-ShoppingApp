a94041d4f3009c3d0f3c6d291b6e63e39d7ff0b3
buisihung11 <38785848+buisihung11@users.noreply.github.com>
2020-04-12 22:13:35 +0700

    Fetch product, pull to refresh from firebase

--- screens/shop/ProductsOverviewScreen.js
+++ screens/shop/ProductsOverviewScreen.js
@@ -1,5 +1,7 @@
-import React from 'react';
-import { FlatList, Button, Platform } from 'react-native';
+import React, { useEffect, useCallback, useState } from 'react';
+import { FlatList, Button, Platform, View, Text } from 'react-native';
+// import axios from 'axios';
+import { db } from '../../store/firebase';
 import ProductItem from '../../components/shop/ProductItem';
 import * as cartActions from '../../store/actions/cart';
 import Colors from '../../constants/Colors';
@@ -7,9 +9,38 @@ import Colors from '../../constants/Colors';
 import { useProduct } from '../../store/reducers/products';
 import { useCart } from '../../store/reducers/cart';
 
+const transformProduct = (products) => {
+  const productsId = Object.keys(products);
+
+  const transformedProducts = [];
+
+  productsId.forEach((id) => {
+    const product = products[id];
+    transformedProducts.push({ id, ...product });
+  });
+
+  return transformedProducts;
+};
+
 const ProductsOverviewScreen = ({ navigation }) => {
-  const [{ availableProducts: products }] = useProduct();
+  const [{ availableProducts }] = useProduct();
+  const [products, setProducts] = useState(null);
+  const [isFetching, setIsFetching] = useState(true);
   const [, dispatch] = useCart();
+
+  const fetchProducts = useCallback(async () => {
+    setIsFetching(true);
+    const data = await db.ref('/products').once('value');
+
+    console.log('data', data);
+    setProducts(transformProduct(data.val()).concat(availableProducts));
+    setIsFetching(false);
+  }, []);
+
+  useEffect(() => {
+    // fetch products
+    fetchProducts();
+  }, [fetchProducts]);
 
   const selectItemHandler = (id, title) => {
     console.log(id, title);
@@ -19,9 +50,26 @@ const ProductsOverviewScreen = ({ navigation }) => {
     });
   };
 
+  if (isFetching)
+    return (
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <Text>Fetching products... 🐱‍🏍</Text>
+      </View>
+    );
+
+  if (products && products.length === 0) {
+    return (
+      <View style={{ flex: 1, justifyContent: 'center' }}>
+        <Text>No products was created</Text>
+      </View>
+    );
+  }
+
   return (
     <FlatList
       data={products}
+      refreshing={isFetching}
+      onRefresh={fetchProducts}
       keyExtractor={(item) => item.id}
       renderItem={(itemData) => (
         <ProductItem
