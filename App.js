08542361c02c2f4764f5d34477bd9318297025ff
buisihung11 <38785848+buisihung11@users.noreply.github.com>
2020-04-14 17:23:02 +0700

    Handle auto login and change screen

--- App.js
+++ App.js
@@ -1,26 +1,31 @@
-import React, { useState } from 'react';
-import { StyleSheet, View } from 'react-native';
+/* eslint-disable import/no-extraneous-dependencies */
+/* eslint-disable import/no-unresolved */
+/* eslint-disable global-require */
+import React, { useState, useEffect } from 'react';
+import { StyleSheet } from 'react-native';
 import { AppLoading } from 'expo';
 import { NavigationContainer } from '@react-navigation/native';
 import ErrorBoundary from 'react-native-error-boundary';
 import * as Font from 'expo-font';
+import { enableScreens } from 'react-native-screens';
 import ShopNavigator from './navigation/ShopNavigator';
 import CartProvider from './store/reducers/cart';
 import OrderProvider from './store/reducers/orders';
 import ProductProvider from './store/reducers/products';
+import UserProvider, { useUser } from './store/reducers/user';
 import AuthScreen from './screens/user/AuthScreen';
 
-const fetchFonts = () => {
-  return Font.loadAsync({
+import useAuth from './components/hooks/useAuth';
+
+enableScreens();
+const fetchFonts = () =>
+  Font.loadAsync({
     'open-sans': require('./assets/fonts/OpenSans-Regular.ttf'),
     'open-sans-bold': require('./assets/fonts/OpenSans-Bold.ttf'),
   });
-};
 
 export default function App() {
   const [fontLoaded, setFontLoaded] = useState(false);
-  const [isLogedIn, setIsLogedIn] = useState(false);
-
   if (!fontLoaded) {
     return (
       <AppLoading
@@ -35,16 +40,31 @@ export default function App() {
     <CartProvider>
       <OrderProvider>
         <ProductProvider>
-          <ErrorBoundary>
-            <NavigationContainer>
-              {isLogedIn ? <ShopNavigator /> : <AuthScreen />}
-            </NavigationContainer>
-          </ErrorBoundary>
+          <UserProvider>
+            <ErrorBoundary>
+              <NavigationContainer>
+                <Main />
+              </NavigationContainer>
+            </ErrorBoundary>
+          </UserProvider>
         </ProductProvider>
       </OrderProvider>
     </CartProvider>
   );
 }
+
+const Main = () => {
+  const { user } = useAuth();
+  const [{ user: userData }, userDispatcher] = useUser();
+
+  useEffect(() => {
+    console.log('USER_SET');
+    userDispatcher({ type: 'SET_USER', user });
+  }, [user]);
+
+  return userData ? <ShopNavigator /> : <AuthScreen />;
+};
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
